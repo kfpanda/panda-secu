b87cc1ac3e4085bf1418041ba32633f1c1f99740
18973119623 <18973119623@189.cn>
2016-03-03 17:39:53 +0800

    模态框

--- html/js/system-setting.js
+++ html/js/system-setting.js
@@ -1,10 +1,17 @@
-$(document).ready(function(){
-	$(".bar").find('a[name=first]').on('click',function(){
+$(document).ready(function() {
+	$(".bar").find('a[name=first]').on('click', function() {
 		$(this).parent().find(".in-ul").slideToggle(600);
 	});
-	 
-	$(".top-right").on("click",function(){
-			$("#logform").slideToggle(600);
+
+	$(".top-right").on("click", function() {
+		$("#logform").slideToggle(600);
 	});
-    
+
+
+	$(window).resize(function() {
+//		var wc = $(window).width() - $(".modal").width()-188;
+//		var hc=$(".bottom").height()+15;
+//		wc = wc / 2;
+//		$(".modal").css('right', wc)
+	});
 });
